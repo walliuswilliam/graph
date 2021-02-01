5efc341630b2d4799d2a17524786151c5136d4c0
walliuswilliam <william.wallius@gmail.com>
2021-02-01 02:11:05 +0000

    fixed/finished breadth first, starting depth first

--- src/graph.py
+++ src/graph.py
@@ -5,16 +5,43 @@ class Node:
     self.neighbors = None
 
 class Graph:
-  def __init__(self, edges):
+  def __init__(self, edges, max_index):
     self.edges = edges
-    
+    self.nodes = [Node(i) for i in range(max_index+1)]
 
-  def get_neighbors(self, node):
-    output_list = []
-    for pair in self.edges:
-      if pair[0] == node.index:
-        output_list.append(Node(pair[1]))
-      if pair[1] == node.index:
-        output_list.append(Node(pair[0]))
-    return output_list
+
+  def build_from_edges(self):
+    for node in self.nodes:
+      neighbor_list = []
+      for pair in self.edges:
+        if pair[0] == node.index:
+          neighbor_list.append(Node(pair[1]))
+        if pair[1] == node.index:
+          neighbor_list.append(Node(pair[0]))
+      node.neighbors = neighbor_list
+
+
+  def get_nodes_breadth_first(self, starting_index):
+    queue = [starting_index]
+    visited = []
+    while len(visited) != len(self.nodes):
+      node = queue[0]
+      queue.remove(node)
+      visited.append(node)
+      for neighbor in [node.index for node in self.nodes[node].neighbors]:
+        if neighbor not in visited and neighbor not in queue:
+          queue.append(neighbor)
+    return visited
+
+  def get_nodes_depth_first(self, starting_index):
+    stack = [starting_index]
+    visited = []
+    while len(stack) != len(self.nodes):
+      node = stack[0]
+      stack.remove(node)
+      visited.append(node)
+      for neighbor in [node.index for node in self.nodes[node].neighbors]:
+        if neighbor not in visited and neighbor not in stack:
+          stack.insert(0, neighbor)
+    return visited
 
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,5 +4,18 @@ from graph import Graph
 from graph import Node
 
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
-graph = Graph(edges)
+graph = Graph(edges, 5)
+graph.build_from_edges()
 
+bf = graph.get_nodes_breadth_first(2)
+
+print('testing breadth first search...')
+assert bf == [2, 1, 0, 3, 4, 5], bf
+print('passed')
+
+
+df = graph.get_nodes_depth_first(2)
+
+print('testing breadth first search...')
+assert df == [2, 1, 3, 4, 5, 0], df
+print('passed')
