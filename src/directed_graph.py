aacb3348bcc0fd31cddd9d4aed4d10b4f383ae32
walliuswilliam <william.wallius@gmail.com>
2021-02-10 03:49:34 +0000

    finished directed graph

--- src/directed_graph.py
+++ src/directed_graph.py
@@ -2,11 +2,12 @@ class Node:
   def __init__(self, index):
     self.index = index
     self.value = None
-    self.neighbors = None
+    self.children = None
+    self.parents = None
     self.previous = None
     self.distance = None
 
-class Graph:
+class DirectedGraph:
   def __init__(self, edges):
     self.edges = edges
     max_index = 0
@@ -16,61 +17,64 @@ class Graph:
       if b > max_index:
         max_index = b
     self.nodes = [Node(i) for i in range(max_index+1)]
-    self.set_neighbors()
+    self.set_families()
 
-  def set_neighbors(self):
+  def set_families(self):
     for node in self.nodes:
-      neighbor_list = []
+      parents_list = []
+      children_list = []
       for pair in self.edges:
         if pair[0] == node.index:
-          neighbor_list.append(self.nodes[pair[1]])
+          children_list.append(self.nodes[pair[1]])
         if pair[1] == node.index:
-          neighbor_list.append(self.nodes[pair[0]])
-      node.neighbors = neighbor_list
+          parents_list.append(self.nodes[pair[0]])
+      node.parents = parents_list
+      node.children = children_list
 
 
-  def get_nodes_breadth_first(self, starting_index):
+  def nodes_breadth_first(self, starting_index):
     for node in self.nodes:
       node.distance = 0
     queue = [self.nodes[starting_index]]
     visited = []
-    while len(visited) != len(self.nodes):
+    while len(queue) != 0:
       node = queue[0]
       queue.remove(node)
       visited.append(node)
-      for neighbor in node.neighbors:
-        if neighbor not in visited and neighbor not in queue:
-          queue.append(neighbor)
-          neighbor.previous = node
-          neighbor.distance = node.distance + 1
+      for child in node.children:
+        if child not in visited and child not in queue:
+          queue.append(child)
+          child.previous = node
+          child.distance = node.distance + 1
     return visited
 
-  def get_nodes_depth_first(self, starting_index):
+  def nodes_depth_first(self, starting_index):
     stack = [self.nodes[starting_index]]
     visited = []
-    while len(visited) != len(self.nodes):
+    while len(stack) != 0:
       node = stack[0]
       stack.remove(node)
       visited.append(node)
-      for neighbor in node.neighbors:
-        if neighbor not in visited and neighbor not in stack:
-          stack.insert(0, neighbor)
+      for child in node.children:
+        if child not in visited and child not in stack:
+          stack.insert(0, child)
     return visited
 
   def calc_distance(self, starting_node_index, ending_node_index):
-    self.get_nodes_breadth_first(starting_node_index)
+    self.nodes_breadth_first(starting_node_index)
     return self.nodes[ending_node_index].distance
 
-
   def calc_shortest_path(self, starting_node_index, ending_node_index):
-    self.get_nodes_breadth_first(starting_node_index)
+    self.nodes_breadth_first(starting_node_index)
     node = self.nodes[ending_node_index]
     shortest_path = [node]
-
+    counter = 0
+    
     while (node != self.nodes[starting_node_index]):
+      if counter > len(self.nodes):
+        return False
       node = node.previous
       shortest_path.append(node)
+      counter += 1
+
     return [node.index for node in reversed(shortest_path)]
-
-
-
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('src')
 from graph import Graph
-from graph import Node
+
 
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
